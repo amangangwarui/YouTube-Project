8cfe9b05ab503b67996e57c1cf71625c8378ff4a
Aman Gangwar <subodhg013@gmail.com>
2026-02-07 00:04:41 +0530

    Connect to Mongo Atlas

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,56 @@
+// import dotenv from "dotenv"
+// import connectDB from "./db/db.js";
+
+
+// dotenv.config({
+//     path: './env'
+// })
+
+// connectDB()
+
+
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/db.js";
+import app from "./app.js";
+
+connectDB();
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error) => {
+        console.log("ERROR:", error);
+        throw error
+       })
+
+       app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+       })
+
+    } catch (error) {
+        console.log("ERROR: ", error)
+        throw err
+    }
+})()
+ 
+*/
